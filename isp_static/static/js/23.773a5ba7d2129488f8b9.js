1b0655d9319765a2304571cf6c9c49b758802c0c
zhangxiaoyang <zhangxiaoyang@yiche.com>
2017-07-24 09:42:15 +0800

    代码修改

--- isp_static/static/js/23.773a5ba7d2129488f8b9.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([23],{134:function(e,t,n){var r=n(61)(n(410),n(519),null,null);e.exports=r.exports},410:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default={}},519:function(e,t){e.exports={render:function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"demo"},[n("router-view")],1)},staticRenderFns:[]}}});
-//# sourceMappingURL=23.773a5ba7d2129488f8b9.js.map
---
+++ src/pages/approval/auditList/AuditList.vue
@@ -0,0 +1,74 @@
+<template lang="html">
+  <div id = "auditList">
+      <Form :label-width="80">
+          <div class="formTop clear">
+              <div class="item fl">
+                  <Form-item label="单据名称:" prop="name">
+                      <Input v-model="searchInfo.chainName" placeholder="请输入审批链名称"></Input>
+                  </Form-item>
+              </div>
+              <div class="item fl">
+                  <Form-item label="用户组名称:" prop="name">
+                      <Input v-model="searchInfo.groupName" placeholder="请输入用户组名称"></Input>
+                  </Form-item>
+              </div>
+              <div class="item fl">
+                  <Form-item label="单据名称:" prop="name">
+                      <Select v-model="searchInfo.formType" style="width:268px">
+                          <Option v-for="item in selectData" :value="item" :key="item.value">{{item}}</Option>
+                      </Select>
+                  </Form-item>
+              </div>
+          </div>
+          <div class="formBot clear">
+              <div class="item fl">
+                  <Form-item label="创建时间:">
+                      <Date-picker type="date" placeholder="选择日期" v-model="formValidate.date" class='dateItem fl'></Date-picker>
+                      <span class='fl line'></span>
+                      <Date-picker type="date" placeholder="选择日期" v-model="formValidate.date" class='dateItem fl'></Date-picker>
+                  </Form-item>
+              </div>
+
+          </div>
+
+          <Form-item class='submitBtn'>
+              <Button type="primary" @click="search" class="search fr">查询</Button>
+          </Form-item>
+      </Form>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+#auditList {
+    .formTop {
+        margin-top: 50px;
+
+        .item {
+            width: 348px;
+             margin-right: 30px;
+        }
+    }
+
+    .formBot {
+        .item {
+            width: 800px;
+        }
+
+        .line {
+            margin: 20px;
+            width: 6px;
+            border-bottom: 1px solid #ccc;
+        }
+
+        .dateItem {
+            width: 268px;
+        }
+    }
+}
+
+</style>
